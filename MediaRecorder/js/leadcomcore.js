44351ff1844a30735dfec494074092fabf059b29
gxz <ganxunzou@163.com>
2020-08-07 15:52:01 +0800

    init

---
+++ MediaRecorder/js/leadcomcore.js
@@ -0,0 +1,64 @@
+﻿(function($){
+	/* 扩展核心API */
+	$.extend({
+		namespace:function(){	//添加命名空间的功能
+			var a=arguments, o=null, i, j, d, rt;
+			for (i=0; i<a.length; ++i) {
+				d=a[i].split(".");
+				rt = d[0];
+				eval('if (typeof ' + rt + ' == "undefined"){' + rt + ' = {};} o = ' + rt + ';');
+				for (j=1; j<d.length; ++j) {
+					o[d[j]]=o[d[j]] || {};
+					o=o[d[j]];
+				}
+			}
+		}
+		,raise:function(){
+			var reFlag =true;
+			var evtHandler,params;
+			evtHandler =arguments[0];
+			try{
+				if (evtHandler==null ||evtHandler=="")return reFlag;
+			}catch(e){
+			}
+			if (typeof(evtHandler)=="function"){
+				reFlag =evtHandler(arguments[1],arguments[2],arguments[3],arguments[4],arguments[5]);
+				if (reFlag==null)reFlag =true;
+				return reFlag;
+			}else if (evtHandler.constructor ==Array ||typeof(evtHandler)=="object"){
+				for (var index=0; index<evtHandler.length; index++){
+					var eFun =evtHandler[index];
+					if (eFun ==null)continue;
+					reFlag =eFun(arguments[1],arguments[2],arguments[3],arguments[4],arguments[5]);
+					if (reFlag==null)reFlag =true;
+					if (!reFlag)break;
+				}
+				return reFlag;
+			}
+			params ="";
+			for(var index=1;index<arguments.length;index++){
+				if (arguments[index]!=null){
+					if (arguments[index].constructor==String) params =params +"'" +escape(arguments[index])+"',";
+					else params =params +"arguments[" +index +"],";
+				}
+				else params =params +"null,";
+			}
+			if (params.length>0) params ="(" +params.substr(0,params.length-1)+")";
+			if (evtHandler.indexOf(";")>-1){
+				var funs =evtHandler.split(';');
+				for(var index=0; index<funs.length; index++){
+					var fun =funs[index];
+					if (fun.indexOf("(")==-1) fun +=params;
+					reFlag =eval(fun);
+					if (reFlag==null)reFlag =true;
+					if (!reFlag)break;
+				}
+			}else{
+				if (evtHandler.indexOf("(")==-1) evtHandler +=params;
+				reFlag =eval(evtHandler);
+			}
+			if (reFlag==null)reFlag =true;
+			return reFlag;
+		}
+	});
+})(jQuery);
---
+++ MediaRecorder/js/leadcomvideo.js
@@ -0,0 +1,73 @@
+﻿jQuery.namespace("leadcom.video");
+
+(function($,δ,win){
+	win.leadcom.video=δ={
+		version:"2.0.1.1",timeHandler:-1,timeRate:500,cometState:-1,events:{},socket:null
+		,result:{"success":0,"fail":-1,"validTelNum":201,"limitTelNum":202,"timeout":100,"nophone":101,"online":102}
+		,video:function(){
+			this.setVideo=function(callback){			//播放视频
+				δ.fn.jsonpRequest({"msgType":"setVideo","params":"{}","message":"设置视频设备"},callback);
+			};
+			this.startVideo=function(filename,callback){		//开始视频
+				δ.fn.jsonpRequest({"msgType":"startVideo","params":"{\"filename\":\""+filename+"\"}","message":"开始视频"},callback);
+			};
+			this.stopVideo=function(callback){			//结束视频
+				δ.fn.jsonpRequest({"msgType":"stopVideo","params":"{}","message":"结束视频"},callback);
+			};
+			this.playVideo=function(exserial,callback){			//播放视频
+				δ.fn.jsonpRequest({"msgType":"playVideo","params":"{\"exserial\":\""+exserial+"\"}","message":"开始播放视频"},callback);
+			};
+			this.deleteVideo=function(exserial,callback){			//删除视频
+				δ.fn.jsonpRequest({"msgType":"deleteVideo","params":"{\"exserial\":\""+exserial+"\"}","message":"删除视频"},callback);
+			};			
+		}
+		,create:function(){		
+			var video = new δ.video();
+			return video;
+		}
+		,fn:{
+			jsonpRequest:function(data,callback,completeback){
+				$.ajax({
+						type: "GET",
+						url: "http://127.0.0.1:9000/webrequest/",						 
+						data: data,						
+						timeout:10000,				//超时10秒
+						async:false,
+						jsonp:"callback",			//服务端用于接收callback调用的function名的参数
+						jsonpCallback:"localCallback",			//callback的function名称
+						dataType:"jsonp",
+						success: function(msg){
+								var args = (msg.content)?$.parseJSON(msg.content):{};
+								if(args.result==-1){
+									args["result"]=args.result;
+									args["message"]=args.message;
+									δ.fn.raiseCallback(args,callback);
+								}else{
+									args["result"]=δ.result.success;
+									args["message"]=data.message;
+									δ.fn.raiseCallback(args,callback);
+								}
+							  },
+					error:function(XHR,status,errorThrown){						
+					},
+					complete:function(XHR,status){						
+						if(completeback)$.raise(completeback,status);
+				　}
+				 });
+			}			
+			,jsonpCallback:function(msg){
+
+			}
+			,raiseCallback:function(args,callback){
+				try{
+					if(callback)$.raise(callback,args);
+				}catch(e){
+					
+				}
+			}
+		}		
+	}
+})(jQuery,leadcom.video,window);
+
+var localCallback = leadcom.video.fn.jsonpCallback;
+
